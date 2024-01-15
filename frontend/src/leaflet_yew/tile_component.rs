1c87fbcf6d7c4cce6b86018fe98a8791dbf62268
chirag-droid <chirag.singla.pi@gmail.com>
2024-01-15 14:31:41 +0000

    fix tile server build string

--- frontend/src/leaflet_yew/tile_component.rs
+++ frontend/src/leaflet_yew/tile_component.rs
@@ -15,7 +15,7 @@ pub fn tile_component(props: &TileComponentProps) -> Html {
     let ctx = use_context::<MapContext>().unwrap();
 
     if let Some(map) = ctx.map.clone() {
-        let tile_layer = TileLayer::new(&url);
+        let tile_layer = TileLayer::new(format!("{url}/{{z}}/{{x}}/{{y}}.png").as_str());
         tile_layer.add_to(&map);
     }
 
