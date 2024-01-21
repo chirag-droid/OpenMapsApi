c0c91ebccc7e4a1bb0c3401061eaf1163243e788
Chirag Singla <chirag.singla.pi@gmail.com>
2024-01-21 19:28:02 +0530

    Update home.rs

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -16,17 +16,10 @@ pub fn home() -> Html {
             <Navigation url={routing_service} from={(28.43891000, 77.00592000)} to={(28.494976, 77.089542)} />
         </MapContainer>
 
-        <div class="bg-white p-4 shadow-md rounded-md mt-4">
-            <h2 class="text-lg font-semibold">{"Reverse Geocode"}</h2>
-            <p class="text-gray-600">{"Find location details by coordinates."}</p>
-            <button class="bg-blue-500 text-white px-4 py-2 mt-2 rounded-md hover:bg-blue-600">{"Try it Now"}</button>
-        </div>
-
-
         <div class="mt-4">
             <h2 class="text-xl font-semibold">{"API Documentation"}</h2>
             <p class="text-gray-600">{"Explore the API using the OpenAPI specification."}</p>
-            <a href="link-to-your-openapi-docs" target="_blank" class="text-blue-500 hover:underline">{"View API Docs"}</a>
+            <a href="/docs" target="_blank" class="text-blue-500 hover:underline">{"View API Docs"}</a>
         </div>
         </main>
     }
