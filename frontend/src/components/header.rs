0fc849459b2a47d320a5d354bb1a7269599a40a4
Chirag Singla <chirag.singla.pi@gmail.com>
2024-01-21 19:28:31 +0530

    Update header.rs

--- frontend/src/components/header.rs
+++ frontend/src/components/header.rs
@@ -5,6 +5,7 @@ pub fn header() -> Html {
     html!(
         <header class="bg-blue-500 p-4 text-white">
             <h1 class="text-2xl font-bold">{"OpenStreetData API"}</h1>
+            <a href="/docs"></a>
         </header>
     )
 }
